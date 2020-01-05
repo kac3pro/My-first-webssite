38037f27562230d8724c715b159d758cdb9e8473
kac3pro <kac3pro@gmail.com>
2020-01-05 15:19:44 +0100

    overlay styled

--- main.js
+++ main.js
@@ -1,32 +1,8 @@
-class product {
-    constructor(image, name, desc) {
-        this.image = image;
-        this.name = name;
-        this.desc = desc;
-    }
-}
-
-
-p = new product("/images/jeans.jpg", "JEANS", "DESCRIPTION");
-
-
-
-
-
-
-
-function generateTemplate(product) {
-    let templateScript = document.getElementById("expression-template").innerHTML;
-    let template = Handlebars.compile(templateScript);
-
-    let context = {
-
-        "description": product.desc
-    };
-
-    //console.log(template(context));
-    document.getElementsByClassName("contentPlaceholder").innerHTML = template(context);
+function toggleOverlay() {
+    let overlay = document.getElementsByClassName("overlay");
+    overlay.style.display = "block";
 
 }
 
-generateTemplate(p);
+
+document.addEventListener("load", toggleOverlay);
