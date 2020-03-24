b9e540d58efea9926e59433eae336caeea765004
kac3pro <kac3pro@gmail.com>
2020-03-24 17:55:51 +0100

    colors

--- products.php
+++ products.php
@@ -13,40 +13,81 @@
 <h1 id="catalogue">Product Catalogue</h1>
 <aside class="filters">
 	<form action="#" onsubmit="">
-		<div role="group" aria-labelledby="size">
-			<label id="size" class="catname">Size:</label>
-			<div class="container" id="size">
+		<label id="size" class="catname">Size:</label>
+		<div class="category" role="group" aria-labelledby="size">
+
+			<div class="container size">
 				<input type="checkbox" class="cb" name="size_xxl" id="size_xxl" checked tabindex="0" aria-checked="true">
 				<span class="checkmark" tabindex="1" role="checkbox"></span>
-				<label for="size_xxl" class="size">XXL</label>
+				<label for="size_xxl" class="cat">XXL</label>
 			</div>
-			<div class="container">
+			<div class="container size">
 				<input type="checkbox" class="cb" name="size_xl" id="size_xl" checked tabindex="0" aria-checked="true">
 				<span class="checkmark" tabindex="1" role="checkbox"></span>
-				<label for="size_xl" class="size">XL</label>
+				<label for="size_xl" class="cat">XL</label>
 			</div>
-			<div class="container">
+			<div class="container size">
 				<input type="checkbox" class="cb" name="size_l" id="size_l" checked tabindex="0" aria-checked="true">
 				<span class="checkmark" tabindex="1" role="checkbox"></span>
-				<label for="size_l" class="size">L</label>
+				<label for="size_l" class="cat">L</label>
 			</div>
-			<div class="container">
+			<div class="container size">
 				<input type="checkbox" class="cb" name="size_m" id="size_m" checked tabindex="0" aria-checked="true">
 				<span class="checkmark" tabindex="1" role="checkbox"></span>
-				<label for="size_m" class="size">M</label>
+				<label for="size_m" class="cat">M</label>
 			</div>
-			<div class="container">
+			<div class="container size">
 				<input type="checkbox" class="cb" name="size_s" id="size_s" checked tabindex="0" aria-checked="true">
 				<span class="checkmark" tabindex="1" role="checkbox"></span>
-				<label for="size_s" class="size">S</label>
+				<label for="size_s" class="cat">S</label>
 			</div>
-			<div class="container">
+			<div class="container size">
 				<input type="checkbox" class="cb" name="size_xs" id="size_xs" checked tabindex="0" aria-checked="true">
 				<span class="checkmark" tabindex="1" role="checkbox"></span>
-				<label for="size_xs" class="size">XS</label>
+				<label for="size_xs" class="cat">XS</label>
 			</div>
 		</div>
+		<hr class="filter-break"/>
+		<label id="color" class="catname">Color:</label>
+		<div class="category" role="group" aria-labelledby="color">
+			<div class="container  color">
+				<input type="checkbox" class="cb" name="color_red" id="color_red" checked tabindex="0" aria-checked="true">
+				<span class="checkmark" tabindex="1" role="checkbox"></span>
+				<label for="color_red" class="cat">Red</label>
+				<div class="color_icon red"></div>
+			</div>
+			<div class="container  color">
+				<input type="checkbox" class="cb" name="color_orange" id="color_orange" checked tabindex="0" aria-checked="true">
+				<span class="checkmark" tabindex="1" role="checkbox"></span>
+				<label for="color_orange" class="cat">Orange</label>
+				<div class="color_icon orange"></div>
+			</div>
+			<div class="container  color">
+				<input type="checkbox" class="cb" name="color_yellow" id="color_yellow" checked tabindex="0" aria-checked="true">
+				<span class="checkmark" tabindex="1" role="checkbox"></span>
+				<label for="color_yellow" class="cat">Yellow</label>
+				<div class="color_icon yellow"></div>
+			</div>
+			<div class="container  color">
+				<input type="checkbox" class="cb" name="color_blue" id="color_blue" checked tabindex="0" aria-checked="true">
+				<span class="checkmark" tabindex="1" role="checkbox"></span>
+				<label for="color_blue" class="cat">Blue</label>
+				<div class="color_icon blue"></div>
+			</div>
+			<div class="container  color">
+				<input type="checkbox" class="cb" name="color_green" id="color_green" checked tabindex="0" aria-checked="true">
+				<span class="checkmark" tabindex="1" role="checkbox"></span>
+				<label for="color_green" class="cat">Green</label>
+				<div class="color_icon green"></div>
+			</div>
+			<div class="container  color">
+				<input type="checkbox" class="cb" name="color_gray" id="color_gray" checked tabindex="0" aria-checked="true">
+				<span class="checkmark" tabindex="1" role="checkbox"></span>
+				<label for="color_gray" class="cat">Gray</label>
+				<div class="color_icon gray"></div>
+			</div>
 
+		</div>
 	</form>
 </aside>
 <div class="flex-container product-search">
